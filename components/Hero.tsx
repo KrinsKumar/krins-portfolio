1269dfaa32369c5dda7bb6c81fbb083ab0413223
Krins <krinskumar@gmail.com>
2023-08-14 17:16:59 -0400

    skills-section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,14 +16,14 @@ const Hero = () => {
     if (!mounted) return null
 
     return (
-        <section className={`relative overflow-hidden before:absolute before:top-0 before:left-1/2 bg-transparent before:w-full before:h-full  before:transform before:-translate-x-1/2`}>
+        <section className={`relative overflow-hidden before:absolute before:top-0 before:left-1/2 bg-transparent before:w-full before:h-full  before:transform before:-translate-x-1/2 flex flex-wrap pt-24`}>
             { theme === 'dark' 
                 ? <Image src="/heroo.svg" alt="Hero" layout="fill" objectFit="cover" quality={100} className='-z-20'/>
                 : <Image src="/hero.svg" alt="Hero" layout="fill" objectFit="cover" quality={100} className='-z-20'/>
             }
-            <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+            <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pb-10">
                 <div className="flex justify-center">
-                <a className={`inline-flex items-center gap-x-2 ${theme == "light" ? "bg-white border border-gray-200 hover:border-gray-300 text-gray-600" : "dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-400"} text-xs  p-2 px-3 rounded-full transition  `} target='_blank' href='https://github.com/KrinsKumar'>
+                <a target='_blank' href='https://github.com/KrinsKumar' className={`z-50 inline-flex items-center border gap-x-2 ${theme == "light" ? "bg-white border-gray-200 hover:border-gray-300 text-gray-600" : "dark:bg-gray-800  dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-400"} text-xs  p-2 px-3 rounded-full transition`}>
                     Explore my Github
                     <span className="flex items-center gap-x-1">
                     <span className={`border-l border-gray-200 text-blue-600 pl-2 ${theme == "dark" ? "dark:text-blue-500" : ""}`}>Explore</span>
@@ -40,11 +40,11 @@ const Hero = () => {
                 </div>
 
                 <div className="mt-5 max-w-3xl text-center mx-auto">
-                <p className={`text-lg text-gray-600 ${theme == "dark" ? "dark:text-gray-400" : ""}`} >Preline is a large open-source project, crafted with Tailwind CSS framework by Hmlstream.</p>
+                <p className={`text-lg text-gray-600 ${theme == "dark" ? "dark:text-gray-400" : ""}`} >I am an enthusiastic, and fast learning software developer student, specializing in full stack development.</p>
                 </div>
 
                 <div className="mt-8 grid gap-3 w-full sm:inline-flex cursor-pointer sm:justify-center">
-                    <a className="inline-flex justify-center items-center gap-x-3 text-center bg-violet-700 border border-transparent text-grey text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-4 dark:focus:ring-offset-gray-800" href="#">
+                    <a className={`inline-flex justify-center items-center gap-x-3 text-center  border border-transparent text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-4 ${theme == "dark" ? " dark:focus:ring-offset-gray-800 bg-indigo-600" : "bg-violet-700 text-white"}`} href="#">
                         Check out my projects!
                     </a>
                 </div>
